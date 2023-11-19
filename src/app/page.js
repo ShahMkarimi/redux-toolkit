c05e26f2f4937f3d0e9e4007ff9c432721963ce5
Mohammad karimi <mk0782212@gmail.com>
2023-11-19 12:03:33 +0430

    change in first page

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="w-full h-screen grid place-items-center text-5xl">
+        Welcome to Next + Redux Projects
+      </h1>
+    </main>
+  );
+}
