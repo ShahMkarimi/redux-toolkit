fba2a6c830d365a1afd9a0fc6bd5fc13970cfd43
Mohammad karimi <mk0782212@gmail.com>
2023-11-19 16:40:03 +0430

    home page bg change

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="w-full bg-black">
+      <h1 className="w-full h-screen grid place-items-center text-5xl">
         Welcome to Next + Redux Projects
       </h1>
     </main>
