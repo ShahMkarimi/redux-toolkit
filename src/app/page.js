2fbd12d5e6198ba72dc4850ffbc91365c9f71862
Mohammad karimi <mk0782212@gmail.com>
2023-11-19 11:43:13 +0430

    home style changed

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="w-full bg-black">
-        Welcome to Next + Redux Projects
+      <h1 className="w-full h-screen grid place-items-center text-5xl">
+        Welcome to Next + Redux toolkit Projects
       </h1>
     </main>
   );
